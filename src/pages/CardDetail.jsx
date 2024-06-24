e0bb55e02e120d5218adb00efb81cd84dedde575
Andrew <huli-gan_94@mail.ru>
2024-06-24 15:09:49 +0300

    Удалил лишний импорт

--- src/pages/CardDetail.jsx
+++ src/pages/CardDetail.jsx
@@ -1,7 +1,5 @@
 import { useParams } from "react-router-dom";
-import { initialProducts } from "../../data.js";
 import { Link } from "react-router-dom";
-import { useState, useEffect } from "react";
 import useProductsStore from "../store/useProductsStore.js";
 
 const CardDetail = () => {
