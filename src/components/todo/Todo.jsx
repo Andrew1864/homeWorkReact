00c5e214243cf627ed4344559795d1e04c0d7792
Andrew <huli-gan_94@mail.ru>
2024-07-14 14:45:49 +0300

    Добавил уведомление на todo

--- src/components/todo/Todo.jsx
+++ src/components/todo/Todo.jsx
@@ -4,6 +4,9 @@ import { useState } from 'react'
 function TodoList() {
     const [todos, setTodos] = useState([]);
     const [inputValue, setInputValue] = useState('');
+    const [isShowAlert, setShowAlert] = useState(false);
+
+    setTimeout((setShowAlert), 3000); // На исчезание Alert 
 
     // Это функция, которая устанавливает состояние inputValue равным значению поля ввода. Вызывается каждый раз, когда изменяется поле ввода.
     function handleChange(e) {
@@ -14,6 +17,7 @@ function TodoList() {
         e.preventDefault();
         setTodos([...todos, inputValue]);
         setInputValue('');
+        setShowAlert(true);
     };
     // Фунцкия удаления 
     function handleDelete(index) {
@@ -31,6 +35,7 @@ function TodoList() {
                     onChange={handleChange} />
                 <button className="bg-indigo-500 hover:bg-indigo-600 text-white font-normal py-2 px-4 rounded" onClick={handleSubmit}>Add Task</button>
             </form>
+            {isShowAlert && <div>Данные были добавлены:</div>}
             <ul className='p-4'>
                 {todos.map((todo, index) => (
                     <li className='w-80 mr-4 py-2 px-3 '>
